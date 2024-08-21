92e272039a77952017f6baff453b854c6e09ae59
Binod Karunanayake <s4025371@cyberserver1head.csit.local>
2024-08-21 14:00:58 +1000

    Add 6-class classification

--- edge-iiot/2-class/2-predict-from-vector-store.py
+++ edge-iiot/2-class/2-predict-from-vector-store.py
@@ -1,29 +1,34 @@
 import pandas as pd
 import os
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-from langchain_chroma import Chroma
-from statistics import mode
-from sklearn.metrics import classification_report
+from tabulate import tabulate
 
 # Load dateset
 df = pd.read_csv(os.getcwd() + '/../../data/edge-iiot/Edge-IIoTset dataset/Selected dataset for ML and DL/ML-EdgeIIoT-dataset.csv', low_memory=False)
+
+# Split dataset to attack and normal
 attack_df = df[df['Attack_label'] == 1]
+normal_df = df[df['Attack_label'] == 0]
+
+# Drop columns
 attack_df = attack_df.drop(columns=['Attack_label', 'Attack_type'])
+normal_df = normal_df.drop(columns=['Attack_label', 'Attack_type'])
+
+# Split dataset into training and test set
 attack_df_train = attack_df.sample(frac=0.8, random_state=42)
 attack_df_test = attack_df.drop(attack_df_train.index)
-
-normal_df = df[df['Attack_label'] == 0]
-normal_df = normal_df.drop(columns=['Attack_label', 'Attack_type'])
 normal_df_train = normal_df.sample(frac=0.8, random_state=42)
 normal_df_test = normal_df.drop(normal_df_train.index)
 
-print("Attack Training set size: ", attack_df_train.shape)
-print("Attack Test set size: ", attack_df_test.shape)
+# Print dataset sizes in a table
+data = [
+    ["Attack", attack_df.shape[0], attack_df_train.shape[0], attack_df_test.shape[0]],
+    ["Normal", normal_df.shape[0], normal_df_train.shape[0], normal_df_test.shape[0]]
+]
+print(tabulate(data, headers=["Atack type", "Total", "Train", "Test"], tablefmt="grid"))
 
-print("Normal Training set size: ", normal_df_train.shape)
-print("Normal Test set size: ", normal_df_test.shape)
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 
-# Load vector store
 embeddings = HuggingFaceEmbeddings()
 vector_store = Chroma(
     collection_name="edge-iiotset",
@@ -33,36 +38,24 @@ retriever = vector_store.as_retriever(
     search_type="mmr", 
     search_kwargs={"k": 5, "fetch_k": 5})
 
-# Predict for attack entries
-sample_size = attack_df_test.shape[0]
+from statistics import mode
+from sklearn.metrics import classification_report
 
+sample_size = 10 # attack_df_test.shape[0]
+
+datasets = {
+    "attack": attack_df_test,
+    "normal": normal_df_test
+}
 y_pred = []
 y_true = []
-for i in range(sample_size):
-    query_document = str(attack_df_test.iloc[i].to_list())
-    similar_documents = retriever.invoke(query_document, filter={"source": "edge-iiotset"})
-    y_true.append(1)
-    if mode([doc.metadata["label"] for doc in similar_documents]) == "attack":
-        y_pred.append(1)
-    else:
-        y_pred.append(0)
 
-print("Classification report for attack entries")
-print(classification_report(y_true, y_pred))
+for attack_type, dataset in datasets.items():
+    sample_size = dataset.shape[0]
+    for i in range(sample_size):
+        query_document = str(dataset.iloc[i].to_list())
+        similar_documents = retriever.invoke(query_document, filter={"source": "edge-iiotset"})
+        y_true.append(attack_type)
+        y_pred.append(mode([doc.metadata["label"] for doc in similar_documents]))
 
-# Predict for normal entries
-sample_size = normal_df_test.shape[0]
-
-y_pred = []
-y_true = []
-for i in range(sample_size):
-    query_document = str(normal_df_test.iloc[i].to_list())
-    similar_documents = retriever.invoke(query_document, filter={"source": "edge-iiotset"})
-    y_true.append(1)
-    if mode([doc.metadata["label"] for doc in similar_documents]) == "normal":
-        y_pred.append(1)
-    else:
-        y_pred.append(0)
-
-print("Classification report for attack entries")
 print(classification_report(y_true, y_pred))
